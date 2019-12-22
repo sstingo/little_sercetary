f3d787524e2f3689ea5cbdca9586a9b248013a50
張允庭 <tiffanychang1006@gmail.com>
2019-12-23 05:40:17 +0800

    對話框視窗

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,32 @@ const dataid = 'F-C0032-001';
 const apiKey = 'CWB-9986681F-B9E6-474D-8458-753CD3B1C344';
 const format = 'JSON';
 
+enum ConfirmAction { ACCEPT, CANCEL }
+enum Locations {
+  Taipei,
+  NewTaipei,
+  Taoyuan,
+  Taichung,
+  Tainan,
+  Kaohsiung,
+  Keelung,
+  Hsinchu,
+  HsinchuCity,
+  Miaoli,
+  Changhua,
+  Nantou,
+  Yunlin,
+  Chiayi,
+  ChiayiCity,
+  Pingtung,
+  Yilan,
+  Hualien,
+  Taitung,
+  Penghu,
+  Kinmen,
+  Lienchiang
+}
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -50,36 +76,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   void initState() {
     super.initState();
-    getData(); ////
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
-        'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/$dataid?Authorization=$apiKey&format=$format');
-
-    String data = response.body;
-    var decodedData = jsonDecode(data);
-    // print(utf8.decode(decodedData));
-    var city =
-        decodedData['cwbopendata']['dataset']['location'][0]['locationName'];
-
-    print(city);
+    // getData(); ////
   }
 
   @override
@@ -99,17 +99,126 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '0',
               style: Theme.of(context).textTheme.display1,
             ),
+            // DropdownList(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          showAlert(context);
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/$dataid?Authorization=$apiKey&format=$format');
+
+    String data = response.body;
+    // var encodedata = Utf8Encoder(data);
+    var decodedData = jsonDecode(data);
+    // var city =
+    //     decodedData['cwbopendata']['dataset']['location'][0]['locationName'];
+    //cwbopendata.dataset.location[0].locationName
+
+    print(decodedData);
+  }
+
+  Future<Locations> showAlert(BuildContext context) async {
+    //跳出視窗
+    return showDialog<Locations>(
+      context: context,
+      barrierDismissible: true, //控制點擊對話框以外的區域是否隱藏對話框
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text('選擇所在地區'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                // Navigator.pop(context, Locations.Taipei);
+                Navigator.of(context).pop(Locations.Taipei);
+              },
+              child: const Text('臺北市'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                // Navigator.pop(context, Locations.NewTaipei);
+                Navigator.of(context).pop(Locations.NewTaipei);
+              },
+              child: const Text('新北市'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                // Navigator.pop(context, Locations.Taoyuan);
+                Navigator.of(context).pop(Locations.Taoyuan);
+              },
+              child: const Text('桃園市'),
+            ),
+            // actions: <Widget>[
+            //   // Center(
+            //   //   child: DropdownList(),
+            //   // ),
+            //   FlatButton(
+            //     child: const Text('取消'),
+            //     onPressed: () {
+            //       Navigator.of(context).pop(ConfirmAction.CANCEL);
+            //     },
+            //   ),
+            //   FlatButton(
+            //     child: const Text('確認'),
+            //     onPressed: () {
+            //       Navigator.of(context).pop(ConfirmAction.ACCEPT);
+            //     },
+            //   ),
+          ],
+        );
+      },
+    );
+  }
 }
+
+// class DropdownList extends StatefulWidget {
+//   @override
+//   _DropdownListState createState() {
+//     return _DropdownListState();
+//   }
+// }
+
+// class _DropdownListState extends State<DropdownList> {
+//   String _value;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: DropdownButton<String>(
+//         items: [
+//           DropdownMenuItem<String>(
+//             child: Text('Item 1'),
+//             value: 'one',
+//           ),
+//           DropdownMenuItem<String>(
+//             child: Text('Item 2'),
+//             value: 'two',
+//           ),
+//           DropdownMenuItem<String>(
+//             child: Text('Item 3'),
+//             value: 'three',
+//           ),
+//         ],
+//         onChanged: (String value) {
+//           setState(() {
+//             _value = value;
+//           });
+//         },
+//         hint: Text('Select Item'),
+//         value: _value,
+//       ),
+//     );
+//   }
+// }
